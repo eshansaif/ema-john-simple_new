18b2f8ccc6c8cf1e8ae36670f98cc6babb3f4421
eshansaif <35582062+eshansaif@users.noreply.github.com>
2023-03-30 22:57:19 +0600

    added cart component and calculation

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Product.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartArrowDown, faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
